1864c55218a9686e3f4164308ec5f3e337c53963
thegreatsantini <lss13@my.fsu.edu>
2018-06-27 23:47:48 -0700

    added stanford web scrapping

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const indexRoute = express.Router();
 const bodyParser = require('body-parser');
+const request = require('request');
+const cheerio = require('cheerio');
+
+
 
 // user model
 // const db = require('../models/user');
@@ -10,8 +14,22 @@ indexRoute.get("/", function (req, res) {
     res.render('index');
 });
 
-indexRoute.get('/event', function(req, res){
-    res.render('eventView')
+indexRoute.get('/stanford', function (req, res) {
+    request('http://events.stanford.edu/', function (error, response, html) {
+        let data = []
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            $('.postcard-text').each(function (i, element) {
+                const a = $(this)
+                const metaData = {}
+                metaData.image = $('img').eq(1).text();
+                metaData.title = $('h3').eq(i).text();
+                metaData.meetingTime = $('p').eq(i + 1).text();
+                data.push(metaData);
+            });
+        }
+        res.status(200).send(data)
+    });
 })
 
 module.exports = indexRoute;
