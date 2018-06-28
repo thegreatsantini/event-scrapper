fe0ead04278f7a352003a2c71fe086960846af59
thegreatsantini <lss13@my.fsu.edu>
2018-06-27 23:44:45 -0700

    added first test

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+var app = require('../server');
+
+describe('GET /', function () {
+    it('should return a 200 response', function (done) {
+        request(app).get('/')
+            .expect(200, done);
+    });
+});
