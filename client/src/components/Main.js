19c5fd7871b08a8afb2714625b431aa6984adee6
thegreatsantini <lss13@my.fsu.edu>
2018-06-28 14:23:02 -0700

    Home component

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,12 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import EventBright from './resultsContainers/EventBright';
 import MeetUp from './resultsContainers/MeetUp';
 import Stanford from './resultsContainers/Stanford';
+import Home from './Home'
 
 class NavLinks extends Component {
     render() {
         return (
             <Switch>
-                {/* <Route exact path='/' component={Home} /> */}
+                <Route exact path='/' component={Home} />
                 <Route path='/stanford' component={Stanford} />
                 <Route path='/eventbright' component={EventBright} />
                 <Route path='/meetup' component={MeetUp} />
