751e717b11cd4a6f8b3104b75877c9d0e6eee77b
thegreatsantini <lss13@my.fsu.edu>
2018-06-28 15:54:20 -0700

    web scraping endpoint testing, and unit testing

--- routes/index.js
+++ routes/index.js
@@ -32,4 +32,25 @@ indexRoute.get('/api/stanford', function (req, res) {
     });
 })
 
+indexRoute.get('/api/eventbright', function(req, res) {
+    let data = [];
+    request('https://www.eventbrite.com/d/wa--seattle/events--this-weekend/?place_id=101730401&internal_ref=login', function (error, response, html) {
+    if ( response.statusCode === 403 ) {
+        res.status(403).send('403 error')
+    }
+
+    else if (!error && response.statusCode === 200) {
+        const $ = cheerio.load(html);
+        let parsedResults = [];
+        console.log('im here')
+        const thing = $('.eds-media-card-content__content__principal');
+        console.log(thing)
+        // $('.eds-media-card-content__content__principal').each(function (i, element) {
+        //     var a = $(this);
+        //     console.log(a.text());
+        // });
+        }
+    })
+})
+
 module.exports = indexRoute;
--- test/index.test.js
+++ test/index.test.js
@@ -8,3 +8,16 @@ describe('GET /', function () {
             .expect(200, done);
     });
 });
+
+// describe('GET /stanford', function(){
+//     it('should return a 200 respone', function(done) {
+//         request(app).get
+//     })
+// })
+
+describe('GET /eventbright', function() {
+    it('should return a 403 response', function (done) {
+        request(app).get('/eventbright').expect(403, done)
+    })
+})
+
