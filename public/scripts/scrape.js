9f5c5269c8b0c96330093271dfea65303c4f8cd1
thegreatsantini <lss13@my.fsu.edu>
2018-06-29 14:20:00 -0700

    several more tests

--- public/scripts/scrape.js
+++ public/scripts/scrape.js
@@ -1,30 +1,25 @@
-var request = require('request');
-// var express = require('express');
-// var router = express.Router();
-let phantom = require('phantom');
-let cheerio = require('cheerio');
+// var request = require('request');
+// // var express = require('express');
+// // var router = express.Router();
+// let phantom = require('phantom');
+// let cheerio = require('cheerio');
 
-/* GET users listing. */
+// /* GET users listing. */
 
-//This is the important bit
-let getData = async function () {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
-    await page.on('onResourceRequested',
+// //This is the important bit
+// let getData = async function () {
+//     const instance = await phantom.create();
+//     const page = await instance.createPage();
 
-        function (requestData) {
-            console.info('Requesting', requestData.url);
-        });
+//     const status = await page.open('https://www.meetup.com/');
+//     const content = await page.property('content');
+//     // console.log(status);
+//     await instance.exit();
+//     return content;
+// };
 
-    const status = await page.open('https://www.meetup.com/');
-    const content = await page.property('content');
-    console.log(content);
-    await instance.exit();
-    return content;
-};
-
-//calls the functionq and console.logs the data
-getData()
+// //calls the functionq and console.logs the data
+// getData()
 // you can use this data with cheerio and make the appropriate selections, loop through and so on
 
 // request('https://www.meetup.com/#', function (error, response, html) {
--- test/index.test.js
+++ test/index.test.js
@@ -12,38 +12,66 @@ describe('GET /', function () {
 
 describe('GET /stanford', function () {
     it('should return a 200 respone', function (done) {
-        request(app).get('/api/stanford').expect(200, done)
+        request(app).get('/api/v1/stanford').expect(200, done)
     })
-    it('shoudl return an array', function(done){
-        request(app).get('/api/stanford').end(function(err, res){
-            expect(res.body).to.be.an('array');
-            done()
+    it('should return an array', function (done) {
+        request(app).get('/api/v1/stanford').end(function (err, res) {
+            expect(res.body).to.be.an('array')
+            done();
         })
     })
-
     it('should return an array with data', function (done) {
-        request(app).get('/api/stanford').end(function (err, res) {
+        request(app).get('/api/v1/stanford').end(function (err, res) {
             expect(res.body).to.not.be.empty
-            done()
+            done();
         })
+    })
+    it('should contain objects', function () {
+        request(app).get('/api/v1/stanford').end(function (err, res) {
+            expect(res.body).to.include.members({});
+            done();
+        })
+    })
+    it('should all be objects', function () {
+        request(app).get('/api/v1/stanford').end(function (err, res) {
+            for ( let i = 0; i < res.body.length; i++ ) {
+                expect(res.body[i]).to.be.an('object')
+                done();
+            }
+        })
+    })
+});
+
+describe('GET /meetup', function () {
+    it('should return a 200 response', function (done) {
+        request(app).get('/api/v1/meetup').expect(200);
+        done();
+    })
+    it('should return an array', function () {
+        request(app).get('/api/v1/meetup').end(function (err, res) {
+            expect(res.body).to.be.an('array');
+            done();
+        })
+    })
+    it('should not be empty', function () {
+        request(app).get('/api/v1/meetup').end(function (err, res) {
+            expect(res.body).to.not.be.empty;
+            done();
+        })
+    })
+    // it('should contain objects', function () {
+        request(app).get('/api/v1/meetup').end(function (err, res) {
+            console.log(res.body) // why isnt this logging? 
+            // expect(res.body).to.include.members({});
+            // done();
+        // })
     })
 })
-
-// describe('## Create task ', function () {
-//     it('should create a task', function (done) {
-//         request(app).post('/tasks').send(task).end(function (err, res) {
-//             expect(res.statusCode).to.equal(200);
-//             expect(res.body.name).to.equal('integration test');
-//             task = res.body;
-//             done();
-//         });
-//     });
-// }); 
-// });
 
 describe('GET /eventbright', function () {
     it('should return a 403 response', function (done) {
-        request(app).get('/api/eventbright').expect(403, done)
+        request(app).get('/api/v1/eventbright').expect(403);
+        done(); // done works more reliably when writen this way
     })
-})
+});
 
